1b0487678ca9e21edd5a8c02fa998d4e9147d649
Fabio <thebloodshadow14@gmail.com>
2023-10-23 16:24:47 +0200

    ADD - createApp

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: "Hello there",
+      img: "https://picsum.photos/400/600",
+    };
+  },
+}).mount("#app");
